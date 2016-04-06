dea7955c3e906a0753811499279531dd41871be5
Will Hennessy <hennessywill@gmail.com>
2016-04-06 03:11:31 -0500

    Implement orientation layout change for game statistics

--- app/src/main/java/com/will/basketballrecorder/GameActivity.java
+++ app/src/main/java/com/will/basketballrecorder/GameActivity.java
@@ -3,6 +3,8 @@ package com.will.basketballrecorder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import java.util.Map;
+
 public class GameActivity extends FragmentActivity {
 
     private String gameName;
@@ -14,6 +16,26 @@ public class GameActivity extends FragmentActivity {
         gameName = getIntent().getStringExtra("game_name");
         fileName = getIntent().getStringExtra("file_name");
         setContentView(R.layout.activity_game);
+
+//        Map<String, Integer> gameStats = GameJsonUtils.getGameStats(getBaseContext(), gameName, fileName);
+//        FGM_COUNT = getStat("score", gameStats);
+//        FGM_ATTEMPTED = FGM_COUNT + getStat("miss", gameStats);
+//        FGM_PERCENT = FGM_COUNT / FGM_ATTEMPTED;
+//        ASSISTS = getStat("assist", gameStats);
+//        REBOUNDS = getStat("rebound", gameStats);
+//        STEALS = getStat("steal", gameStats);
+//        FOULS = getStat("foul", gameStats);
+//        POINTS_SCORED = FGM_COUNT * 2;
+//
+//
+//        TextView points_made = (TextView) findViewById(R.id.points_scored);
+//        points_made.setText(POINTS_SCORED+"");
+//
+//        TextView assists = (TextView) findViewById(R.id.assists);
+//        assists.setText(ASSISTS+"");
+//
+//        TextView steals = (TextView) findViewById(R.id.steals);
+//        steals.setText(STEALS+"");
     }
 
     public String getGameName() {
@@ -21,5 +43,12 @@ public class GameActivity extends FragmentActivity {
     }
 
     public String getFileName() { return fileName; }
+
+    private static int getStat(String action, Map<String, Integer> gameStats) {
+        if (gameStats.containsKey(action))
+            return gameStats.get(action);
+        return 0;
+    }
+
 
 }
